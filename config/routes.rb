630b54f944366a07a75e16cd03148a8fdbcb4384
Isabela Nery <isabelanery.r@gmail.com>
2025-09-11 15:11:30 -0300

    fix: remove create/update/delete routes for menus

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :menus do
+    resources :menus, only: %i[index show] do
       resources :menu_items, only: %i[index show]
     end
   end
